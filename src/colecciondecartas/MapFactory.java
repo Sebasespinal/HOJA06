51e437478437bfee6b944afd70de8458dddcba8d
Sebasespinal <ezsebastian400@gmail.com>
2024-03-11 18:21:08 -0600

    Clase MapFactory
     En la clase MapFactory, implementamos un patrón de diseño llamado Factory, que permite seleccionar dinámicamente la implementación de la interfaz Map que se utilizará en nuestro programa en tiempo de ejecución. Esto significa que el usuario puede elegir entre diferentes implementaciones de Map (como HashMap, TreeMap y LinkedHashMap) según sus necesidades.
    
    Para lograr esto, creamos un método llamado getMap() que devuelve un objeto Map según la elección del usuario. Primero, mostramos al usuario las opciones disponibles de implementaciones de Map (1 para HashMap, 2 para TreeMap, 3 para LinkedHashMap). Luego, le pedimos al usuario que ingrese el número correspondiente a la implementación deseada.
    
    Utilizamos un Scanner para obtener la entrada del usuario y luego usamos un bloque switch para seleccionar la implementación de Map correspondiente según la elección del usuario. Si el usuario elige una opción inválida, seleccionamos HashMap como la implementación por defecto.
    
    Finalmente, devolvemos la implementación seleccionada de Map, lo que permite al programa principal utilizarla para manejar la colección de cartas. De esta manera, nuestra clase MapFactory actúa como un punto central para crear diferentes implementaciones de Map.

--- src/colecciondecartas/MapFactory.java
+++ src/colecciondecartas/MapFactory.java
@@ -1,5 +1,35 @@
 package colecciondecartas;
 
-class MapFactory {
+import java.util.*;
 
+class MapFactory {
+    private Scanner scanner;
+
+	public Map<String, String> getMap() {
+        scanner = new Scanner(System.in);
+        System.out.println("Seleccione la implementación de Map que desea utilizar:");
+        System.out.println("1. HashMap");
+        System.out.println("2. TreeMap");
+        System.out.println("3. LinkedHashMap");
+        System.out.print("Ingrese el número correspondiente a la implementación deseada: ");
+        
+        int choice = 0;
+        try {
+            choice = Integer.parseInt(scanner.nextLine()); // Lee la opción ingresada por el usuario
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Ingrese un número válido.");
+        }
+        
+        switch (choice) {
+            case 1:
+                return new HashMap<>();
+            case 2:
+                return new TreeMap<>();
+            case 3:
+                return new LinkedHashMap<>();
+            default:
+                System.out.println("Opción inválida. Seleccionando HashMap por defecto.");
+                return new HashMap<>();
+        }
+    }
 }
